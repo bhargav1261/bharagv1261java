17900f4bf4e18fdc80aa82b69540be719d05f0c9
Bhargav <Bhargav@Bhargav>
2022-08-09 13:06:54 +0530

    commit

--- hello/src/hello/A.java
+++ hello/src/hello/A.java
@@ -2,13 +2,13 @@ package hello;
 
 public class A {
 	
-	int a;
-	int b;
+	int c;
+	int d;
 	
 	void display()
 	{
-		System.out.println(a);
-		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
